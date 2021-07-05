98254e8a34ce52e42a188f1cfd48b07c4a2bcf8b
mromauricio <mromauricio@gmail.com>
2021-07-05 18:16:19 -0300

    wip

--- Backend/service.js
+++ Backend/service.js
@@ -12,16 +12,9 @@ exports.alteraUsuario = async (conteudoBody, id) => {
 }
 
 exports.buscaUsuarios = async (nome, email, tel) => {
-
     nome =  nome.includes('*') ?  nome.replace(/\*/g,'%') : `%${nome}%`
-    // if ( nome.includes('*') ) {
-    //     nome = nome.replace(/\*/g,'%') 
-    // } else {
-    //     nome = `%${nome}%`
-    // }
     email =  email.includes('*') ?  email.replace(/\*/g,'%') : `%${email}%`
     tel =  tel.includes('*') ?  tel.replace(/\*/g,'%') : `%${tel}%`
-    
     return await dao.listaUsuariosFiltro(nome, email, tel)
 }
 
